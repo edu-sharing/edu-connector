d4057f43d8fd35bf04b96ffe6b04d399c4639e7c
Steffen Hippeli <hippeli@edu-sharing.com>
2017-05-19 10:20:48 +0200

    add loggin to rlk

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,29 @@
 <?php
 
-$log = new \Monolog\Logger();
+
 /*
- * @see https://odolbeau.fr/blog/when-monolog-meet-elk.html
- */
+ * Log to local file
+ * */
+$log = new \Monolog\Logger();
 $log->pushHandler(new Monolog\Handler\RotatingFileHandler(__DIR__ . '/log/connector.log', 0, \Monolog\Logger::DEBUG));
 $log->pushProcessor(new Monolog\Processor\IntrospectionProcessor());
+
+
+/*
+ * Log to logstash
+ */
+use Monolog\Logger;
+use Monolog\Handler\RedisHandler;
+use Monolog\Formatter\LogstashFormatter;
+use Predis\Client;
+
+
+$redisHandler = new RedisHandler(new Client('tcp://78.46.71.7:6379'), 'phplogs');
+$formatter = new LogstashFormatter('eduConnector');
+$redisHandler->setFormatter($formatter);
+$logger = new Logger('eduConnector', array($redisHandler));
+
+// test
+$logger->info('eduConnector bootstraped.');
+
+$logger->error('eduConnector test an error.');
