89a31d47582245ced13a107dc26e6ae3bef53542
Steffen Hippeli <hippeli@edu-sharing.com>
2017-05-23 12:45:08 +0200

    merge

--- src/tools/tinymce/TinyMce.php
+++ src/tools/tinymce/TinyMce.php
@@ -18,22 +18,12 @@ class TinyMce {
     }
 
     private function setContent() {
-       $_SESSION['content'] = file_get_contents($_SESSION['node']->node->contentUrl);
+        $_SESSION['content'] = file_get_contents($_SESSION['node']->node->contentUrl);
     }
 
     public function setNode()
     {
         $node = $this->apiClient->getNode($_SESSION['node']);
-        if (in_array('Write', $node->node->access)) {
-            $_SESSION['edit'] = true;
-        } else {
-            $_SESSION['edit'] = false;
-        }
-
-        if ($node->node->size === NULL) {
-            $this->apiClient->createContentNode($_SESSION['node'], STORAGEPATH . '/templates/init.' . $_SESSION['filetype'], \connector\tools\onlyoffice\OnlyOffice::getMimetype($_SESSION['filetype']));
-            $node = $this->apiClient->getNode($_SESSION['node']);
-        }
         $_SESSION['node'] = $node;
     }
 
@@ -43,31 +33,4 @@ class TinyMce {
         exit();
     }
 
-    public static function getMimetype($doctype)
-    {
-        switch ($doctype) {
-            case 'docx':
-                $mimetype = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-                break;
-            case 'xlsx':
-                $mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-                break;
-            case 'pptx':
-                $mimetype = 'application/vnd.openxmlformats-officedocument.presentationml.presentation';
-                break;
-            case 'odt':
-                $mimetype = 'application/vnd.oasis.opendocument.text';
-                break;
-            case 'ods':
-                $mimetype = 'application/vnd.oasis.opendocument.spreadsheet';
-                break;
-            case 'odp':
-                $mimetype = 'application/vnd.oasis.opendocument.presentation';
-                break;
-            default:
-                error_log('No mimetype specified');
-                throw new Exception();
-        }
-        return $mimetype;
-    }
 }
