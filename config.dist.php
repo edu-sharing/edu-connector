eec1a712832404f23b7b30655f81909929858ace
Steffen Hippeli <hippeli@edu-sharing.com>
2017-08-09 08:54:15 +0200

    fix config.dist fix http status check to allow 30x

--- config.dist.php
+++ config.dist.php
@@ -3,7 +3,7 @@ define('WWWURL', '');
 define('DOCROOT', '');
 
 /*onlyoffice*/
-define('ONLYOFFICE_EDITORURL', '/src/tools/onlyoffice/doceditor.php');
+define('ONLYOFFICE_EDITORURL', WWWURL . '/src/tools/onlyoffice/doceditor.php');
 define('ONLYOFFICE_STORAGEFOLDER', 'storage');
 define('ONLYOFFICE_STORAGEPATH', DOCROOT . '/src/tools/onlyoffice/' . ONLYOFFICE_STORAGEFOLDER);
 define('ONLYOFFICE_DOCUMENT_SERVER', '');
