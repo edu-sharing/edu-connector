d7855f8cbe8611ad64fd92706aed0db22552549a
chr-ernst <ernst@edu-sharing.net>
2020-02-13 16:01:12 +0100

    fix: added version_info.php

---
+++ install/version_info.php
@@ -0,0 +1,5 @@
+<?php
+
+define('VERSION', '5.1');
+define('BUILD_DATE', '20200213'); //YYYYMMDD
+define('BUILD_COMMIT', '');
--- version.php
+++ version.php
@@ -1,7 +1,9 @@
 <?php
 header('Content-Type: application/json');
-require_once('config.php');
+require_once('install/version_info.php');
 
 $info["version"] = VERSION;
+$info["build_date"] = BUILD_DATE;
+$info["build_commit"] = BUILD_COMMIT;
 
 echo json_encode($info);
