c433c54aa7a4fec9b0b240670c9fa4dbbfd22a50
Torsten Simon <simon@edu-sharing.net>
2020-02-14 10:14:14 +0100

    ci:log branch

--- install/version_info.php
+++ install/version_info.php
@@ -4,3 +4,4 @@ define('VERSION', '5.1');
 // replaced by gitlab ci
 define('BUILD_DATE', '#BUILD_DATE'); //YYYYMMDD
 define('BUILD_COMMIT', '#BUILD_COMMIT');
+define('BUILD_BRANCH', '#BUILD_BRANCH');
--- version.php
+++ version.php
@@ -5,5 +5,6 @@ require_once('install/version_info.php');
 $info["version"] = VERSION;
 $info["build_date"] = BUILD_DATE;
 $info["build_commit"] = BUILD_COMMIT;
+$info["build_branch"] = BUILD_BRANCH;
 
 echo json_encode($info);
