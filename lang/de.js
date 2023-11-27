3b302dd2631196877a197edde6c633f3f0666e30
Torsten Simon <simon@edu-sharing.net>
2023-11-27 15:17:32 +0100

    adopt timeout to es

--- lang/de.js
+++ lang/de.js
@@ -3,7 +3,7 @@ language = {
     functiondeactivatedtext: "Aus Sicherheitsgründen ist diese Funktion deaktiviert.<br/>Öffnen Sie den Editor erneut aus dem Workspace.",
 
     timeoutheading: "Sie wurden abgemeldet",
-    timeouttext: "Ihre Sitzung wurde aufgrund von Inaktivität zu Ihrer Sicherheit automatisch beendet, da Sie mindestens 10 Minuten nicht aktiv waren.<br/>Der Dateiinhalt wurde automatisch gespeichert.<br/>Bitte schließen Sie den Editor und melden Sie sich erneut an.",
+    timeouttext: "Ihre Sitzung wurde aufgrund von Inaktivität zu Ihrer Sicherheit automatisch beendet, da Sie mindestens 5 Minuten nicht aktiv waren.<br/>Der Dateiinhalt wurde automatisch gespeichert.<br/>Bitte schließen Sie den Editor und melden Sie sich erneut an.",
 
     invalidsessionheading: "Session ungültig",
     invalidsessiontext: "Ihre Session ist nicht mehr gültig.<br/>Bitte schließen Sie den Editor und melden Sie sich erneut an.",
