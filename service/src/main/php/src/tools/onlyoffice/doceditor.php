918beba0ac26ba98eb9a3685fc82f5090193bcf0
Torsten Simon <simon@edu-sharing.net>
2025-05-07 22:51:31 +0200

    do not autostart plugin to prevent randomly loading

--- service/src/main/php/src/tools/onlyoffice/doceditor.php
+++ service/src/main/php/src/tools/onlyoffice/doceditor.php
@@ -270,7 +270,7 @@ $_SESSION['id_' . getDocEditorKey($id)] = $id;
             if (ONLYOFFICE_EDUSHARING_PLUGIN === true) {
                 $payload["editorConfig"]["plugins"] = [
                     "autostart" => [
-                        "asc.{b3a20a66-c974-4aa0-8bce-c691d00d558c}",
+                        //"asc.{b3a20a66-c974-4aa0-8bce-c691d00d558c}",
                     ],
                     "pluginsData" => [
                         WWWURL . "/js/only-office-edu-sharing-plugin/config.json.php?id=" . $id
